5e7283e635a0d5a3be864eb36165b0b7297e4c2d
ruthran011 <aathan.13@cse.mrt.ac.lk>
2017-03-27 01:49:24 +0530

    random_2

--- php/GetData.php
+++ php/GetData.php
@@ -11,10 +11,88 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+    if(isset($_POST['username']) && isset($_POST['pw'])){
+
+        $uname = $_POST['username'];
+        $pw = $_POST['pw'];
+
+        $sql1 = "SELECT User_name, Password FROM Student";
+        $result1 = $conn->query($sql1);
+
+        while($row = $result1->fetch_assoc()) {
+            if($row['User_name']==$uname && $row['Password']==$pw){
+                    header("Location:http://localhost:63342/Deedoo/html/User_profile.html?_ijt=ocqn3j8rd3mr8jg5pqfoipqd0");
+                exit();
+            }
+        }
+
+
+        $sql2 = "SELECT University_un, pw FROM University";
+        $result2 = $conn->query($sql2);
+
+        while($row = $result2->fetch_assoc()) {
+            if($row['University_un']==$uname && $row['pw']==$pw){
+                header("Location:http://localhost:63342/Deedoo/html/User_profile.html?_ijt=ocqn3j8rd3mr8jg5pqfoipqd0");
+                exit();
+            }
+        }
+
+
+        $sql3 = "SELECT user_name, password FROM Lecturer";
+        $result3 = $conn->query($sql3);
+
+        while($row = $result3->fetch_assoc()) {
+            if($row['user_name']==$uname && $row['password']==$pw){
+                header("Location:http://localhost:63342/Deedoo/html/User_profile.html?_ijt=ocqn3j8rd3mr8jg5pqfoipqd0");
+                exit();
+            }
+        }
+    }
+
+
+    if(isset($_POST['username']) && isset($_POST['email']) && isset($_POST['pw'])){
+        $u_name = $_POST['username'];
+        $email = $_POST['email'];
+        $pw = $_POST['pw'];
+
+        $sql1 = "SELECT pw FROM University WHERE University_un = '$u_name'";
+        $result1 = $conn->query($sql1);
+
+        if($result1->num_rows > 0){
+            $sql = "UPDATE University SET Admin_pw = '$pw' WHERE Admin_un = '$u_name'";
+            header("Location:http://localhost/Deedoo/html/SignIn_page.html");
+            exit();
+        }
+
+//        while($row = $result1->fetch_assoc()) {
+//            var_dump($row);
+//            if($row['University_un']==$u_name && $row['pw']==$pw && $row['University_email']==$email){
+//
+//            }
+//        }
+
+        $sql2 = "SELECT user_name, email, password FROM Lecturer";
+        $result2 = $conn->query($sql2);
+
+        while($row = $result2->fetch_assoc()) {
+            if($row['user_name']==$u_name && $row['password']==$pw && $row['email']==$email){
+                $sql = "UPDATE Lecturer SET password = '$pw' WHERE user_name = '$u_name'";
+                header("Location:http://localhost/Deedoo/html/SignIn_page.html");
+                exit();
+            }
+        }
+
+        $sql3 = "SELECT User_name, Email, Password FROM Student";
+        $result3 = $conn->query($sql3);
+
+        while($row = $result3->fetch_assoc()) {
+            if($row['User_name']==$u_name && $row['Password']==$pw && $row['Email']==$email){
+                $sql = "UPDATE University SET Password = '$pw' WHERE User_name = '$u_name'";
+                header("Location:http://localhost/Deedoo/html/SignIn_page.html");
+                exit();
+            }
+        }
+
     }
 
     $conn->close();
--- php/InsertData.php
+++ php/InsertData.php
@@ -5,47 +5,6 @@
 //    ini_set('error_log', dirname(__FILE__).'/log.txt');
 //    error_reporting(E_ALL);
 
-    if (isset($_POST["uni_name"]) && isset($_POST["uni_username"]) && isset($_POST["email"]) && isset($_POST["admin_name"]) &&
-        isset($_POST["admin_username"]) && isset($_POST["admin_pw"])){
-
-        $uni_name = ($_POST["uni_name"]);
-        $uni_username = $_POST["uni_username"];
-        $email = $_POST["email"];
-        $admin_name = $_POST["admin_name"];
-        $admin_username = $_POST["admin_username"];
-        $admin_pw = $_POST["admin_pw"];
-
-        $sql = "INSERT INTO University (Uni_name , University_un, University_email, Faculty, Department, Module, Admin_name, Admin_un, Admin_pw)
-                VALUES ('$uni_name', '$uni_username', '$email','kk','kk','admin_name', '$admin_name', '$admin_username', '$admin_pw')";
-    }
-
-    elseif (isset($_POST['lec_name']) && $_POST['profession'] && $_POST['contact_no'] && $_POST['lec_username'] &&
-            $_POST['lec_pw'] && $_POST['lec_email']){
-
-        $lec_name = ($_POST["lec_name"]);
-        $profession = $_POST["profession"];
-        $contact_no = $_POST["contact_no"];
-        $lec_username = $_POST["lec_username"];
-        $lec_pw = $_POST["lec_pw"];
-        $lec_email = $_POST["lec_email"];
-
-        $sql = "INSERT INTO Lecturer (Name , Profession, University, Faculty, Department, Subject, Contact_no, user_name, password, email)
-                VALUES ('$lec_name','$profession','','','','', '$contact_no', '$lec_username', '$lec_pw', '$lec_email')";
-    }
-
-    elseif (isset($_POST['stu_name']) && $_POST['index_no'] && $_POST['stu_username'] && $_POST['stu_pw'] &&
-            $_POST['stu_email']){
-
-        $stu_name = ($_POST["stu_name"]);
-        $stu_username = $_POST["stu_username"];
-        $stu_pw = $_POST["stu_pw"];
-        $stu_email = $_POST["stu_email"];
-        $index_no = $_POST["index_no"];
-
-        $sql = "INSERT INTO Student (Name , University, Faculty, Department, Subject, user_name, password, email, Index_no)
-                VALUES ('$stu_name', '', '', '', '', '$stu_username', '$stu_pw', '$stu_email', '$index_no')";
-    }
-
     $servername = "localhost";
     $username = "root";
     $password = "ruthran011";
@@ -57,10 +16,99 @@
         die("Connection failed: " . $conn->connect_error);
     }
 
-    if ($conn->query($sql) === TRUE) {
-        echo "New record created successfully";
-    } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+    if (isset($_POST["uni_name"]) && isset($_POST["uni_username"]) && isset($_POST["email"]) && isset($_POST["pw"])){
+
+        $uni_name = ($_POST["uni_name"]);
+        $uni_username = $_POST["uni_username"];
+        $email = $_POST["email"];
+        $pw = $_POST["pw"];
+
+        $post = array($_POST);
+        $new_data = array();
+
+        foreach($post[0] as $x => $x_value) {
+            if($x_value!=''){
+                array_push($new_data, $x, $x_value);
+            }
+        }
+
+        $faculty = array();
+        $department = array();
+        $module = array();
+
+        for ($x=0; $x < count($new_data); $x+=2){
+            if(strpos($new_data[$x], 'faculty') !== false){
+                array_push($faculty, $new_data[$x+1]);
+            }elseif (strpos($new_data[$x], 'department') !== false){
+                array_push($department, $new_data[$x+1]);
+            }elseif (strpos($new_data[$x], 'module') !== false){
+                array_push($module, $new_data[$x+1]);
+            }
+        }
+
+        $sql = "INSERT INTO University (Uni_name , University_un, University_email, pw)
+                VALUES ('$uni_name', '$uni_username', '$email', '$pw')";
+
+        $conn->query($sql);
+
+        if(count($faculty)>0){
+            for ($x=0; $x < count($faculty); $x++){
+                $sql = "INSERT INTO Faculty (university_un, faculty) VALUES ('$uni_username', '$faculty[$x]')";
+                $conn->query($sql);
+            }
+        }
+
+        if(count($department)>0){
+            for ($x=0; $x < count($department); $x++){
+                $sql = "INSERT INTO Department (university_un, department) VALUES ('$uni_username', '$department[$x]')";
+                $conn->query($sql);
+            }
+        }
+
+        if(count($module)>0){
+            for ($x=0; $x < count($module); $x++){
+                $sql = "INSERT INTO Module (university_un, module) VALUES ('$uni_username', '$module[$x]')";
+                $conn->query($sql);
+            }
+        }
+
+        header("location:http://localhost/Deedoo/html/SignIn_page.html");
+
+    }
+
+    elseif (isset($_POST['lec_name']) && $_POST['profession'] && $_POST['contact_no'] && $_POST['lec_username'] &&
+            $_POST['pw'] && $_POST['lec_email']){
+
+        $lec_name = ($_POST["lec_name"]);
+        $profession = $_POST["profession"];
+        $contact_no = $_POST["contact_no"];
+        $lec_username = $_POST["lec_username"];
+        $lec_pw = $_POST["pw"];
+        $lec_email = $_POST["lec_email"];
+
+        $sql = "INSERT INTO Lecturer (Name , Profession, Contact_no, user_name, password, email)
+                VALUES ('$lec_name','$profession', '$contact_no', '$lec_username', '$lec_pw', '$lec_email')";
+        $conn->query($sql);
+
+        header("location:http://localhost/Deedoo/html/SignIn_page.html");
+
+    }
+
+    elseif (isset($_POST['stu_name']) && $_POST['Contact_no'] && $_POST['stu_username'] && $_POST['pw'] &&
+            $_POST['stu_email']){
+
+        $stu_name = ($_POST["stu_name"]);
+        $stu_username = $_POST["stu_username"];
+        $stu_pw = $_POST["pw"];
+        $stu_email = $_POST["stu_email"];
+        $contact_no = $_POST["Contact_no"];
+
+        $sql = "INSERT INTO Student (Name , user_name, password, email, Contact_no)
+                VALUES ('$stu_name', '$stu_username', '$stu_pw', '$stu_email', '$contact_no')";
+        $conn->query($sql);
+
+        header("location:http://localhost/Deedoo/html/SignIn_page.html");
+
     }
 
     $conn->close();
